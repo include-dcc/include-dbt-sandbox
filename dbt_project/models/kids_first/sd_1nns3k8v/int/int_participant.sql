1f301289e085a427b82737124906867d0dd6bf4f
Christina Diaz <diazchristina.j@gmail.com>
2025-12-12 18:06:54 -0500

    🎨 Add indents

--- dbt_project/models/kids_first/sd_1nns3k8v/int/int_participant.sql
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/int_participant.sql
@@ -3,19 +3,19 @@
 ) }}
 
 select distinct 
-study_id as study_id, 
-lower(replace(study_id, '_', '-')) as dewrangle_study_id, 
-kf_id as participant_id, 
-lower(replace(kf_id, '_', '-')) as dewrangle_participant_id, 
-external_id,
-family_id, 
-is_proband,
-race,
-ethnicity,
-gender,
-affected_status,
-species,
-visible,
-visibility_reason, -- can we standardize this a bit more? maybe release status instead of reason? and try to standardize more? 
-visibility_comment
+    study_id as study_id, 
+    lower(replace(study_id, '_', '-')) as dewrangle_study_id, 
+    kf_id as participant_id, 
+    lower(replace(kf_id, '_', '-')) as dewrangle_participant_id, 
+    external_id,
+    family_id, 
+    is_proband,
+    race,
+    ethnicity,
+    gender,
+    affected_status,
+    species,
+    visible,
+    visibility_reason, -- can we standardize this a bit more? maybe release status instead of reason? and try to standardize more? 
+    visibility_comment
 from {{ ref('src_participant')}}
