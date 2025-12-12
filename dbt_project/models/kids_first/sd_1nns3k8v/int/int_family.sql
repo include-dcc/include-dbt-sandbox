4e97f4776f2011df3b38b3abd24765cf1bedfa2a
Christina Diaz <diazchristina.j@gmail.com>
2025-12-12 18:06:29 -0500

    🎨 Add indents

--- dbt_project/models/kids_first/sd_1nns3k8v/int/int_family.sql
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/int_family.sql
@@ -3,11 +3,11 @@
 ) }}
 
 select distinct 
-kf_id as family_id, 
-lower(replace(kf_id, '_', '-')) as dewrangle_family_id,
-external_id, 
-family_type, -- not historically populated but it should be; can use logic rules to calculate
-visible,
-visibility_reason,
-visibility_comment
+    kf_id as family_id, 
+    lower(replace(kf_id, '_', '-')) as dewrangle_family_id,
+    external_id, 
+    family_type, -- not historically populated but it should be; can use logic rules to calculate
+    visible,
+    visibility_reason,
+    visibility_comment
 from {{ ref('src_family')}}
