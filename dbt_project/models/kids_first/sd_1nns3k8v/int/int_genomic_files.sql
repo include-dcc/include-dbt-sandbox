f2d7e4c45f32e3af18d7c1f6061b670fd8259aea
Amanda Warkow <warkowa@chop.edu>
2025-12-16 19:25:41 -0500

    ✨ add int GF and SE models

---
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/int_genomic_files.sql
@@ -0,0 +1,44 @@
+{{ config(
+    schema='int'
+) }}
+
+select distinct 
+    kf_id as genomic_file_id,
+    created_at, -- think these could be useful when answering questions about changes over time
+    modified_at, -- ^^^
+    external_id,
+    is_harmonized,
+    reference_genome,
+    controlled_access, -- would be nice to incorporate mappung logic for this field based on file type, location and harmonization
+    availability,
+    paired_end,
+    visible,
+    visibility_reason,
+    visibility_comment,
+
+    -- should we discuss with bix about standardizing these values? I know ingest mapping logic has changed over time
+    data_type, 
+    file_format,
+    data_category,
+    workflow_tool,
+    workflow_type,
+    workflow_version,
+    workflow_endpoint,
+    file_version_descriptor, -- should discuss with bix about reliability of these values in dataservice currently
+
+    -- could be useful after delivery but would be null during source load
+    cavatica_file_id, 
+    cavatica_volume,
+
+    -- is sdc relevant here?
+    -- _sdc_batched_at,
+    -- _sdc_extracted_at,
+    -- _sdc_deleted_at,
+    -- _sdc_received_at,
+    -- _sdc_table_version,
+    -- _sdc_sequence,
+    -- _sdc_source_file,
+    -- _sdc_source_lineno,
+    -- _sdc_sync_started_at,
+
+from {{ ref('src_genomic_file') }}
---
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/int_sequencing_experiments.sql
@@ -0,0 +1,71 @@
+{{ config(
+    schema='int'
+) }}
+
+select distinct 
+    kf_id as sequencing_experiment_id, 
+    sequencing_center_id,
+    external_id,
+    experiment_date,
+    experiment_strategy,
+    is_paired_end,
+    platform,
+    instrument_model,
+    visible,
+    visibility_reason, 
+    visibility_comment
+
+  -- I think these should be included? talk with BIX
+    library_name,
+    library_strand,
+    library_prep,
+    library_selection,
+    max_insert_size,
+    mean_insert_size,
+    mean_depth,
+    total_reads,
+    mean_read_length,
+
+     /*
+        TODO: discuss with bix necessary fields to include
+      */
+    -- adapter_sequencing,
+    -- is_adapter_trimmed,
+    -- read_pair_number,
+    -- target_capture_kit,
+    -- acquisition_type,
+    -- cdna_read,
+    -- cdna_read_offset,
+    -- cell_barcode_offset,
+    -- cell_barcode_read,
+    -- cell_barcode_size,
+    -- chromatography_approach,
+    -- end_bias,
+    -- enrichment_approach,
+    -- fraction_number,
+    -- fractionation_approach,
+    -- ion_fragmentation,
+    -- library_construction,
+    -- mass_spec_rawfile_conversion,
+    -- proteomics_experiment,
+    -- quantification_label_id,
+    -- quantification_labeling_method,
+    -- quantification_technique,
+    -- sequencing_mode,
+    -- target_cell_number,
+    -- umi_barcode_offset,
+    -- umi_barcode_read,
+    -- umi_barcode_size,
+
+    -- are these relevant? not sure what sdc is
+    -- _sdc_batched_at,
+    -- _sdc_extracted_at,
+    -- _sdc_deleted_at,
+    -- _sdc_received_at,
+    -- _sdc_table_version,
+    -- _sdc_sequence,
+    -- _sdc_source_file,
+    -- _sdc_source_lineno,
+    -- _sdc_sync_started_at,
+
+from {{ ref('src_sequencing_experiment')}}
