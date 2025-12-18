d75e25cf99c54bfe66c4baf29b7b6c7a35712796
Christina Diaz <diazchristina.j@gmail.com>
2025-12-18 17:13:25 -0500

    ✨ Add more fields

--- dbt_project/models/access/study.sql
+++ dbt_project/models/access/study.sql
@@ -10,7 +10,7 @@
 select distinct 
 s.study_id, -- do we want to use the KF study id or the dewrangle one? 
 'X01' as funding_source, -- is this the funding source for every kf study? 
-name as study_title, -- is study title the full study name? 
+s.name as study_title, -- is study title the full study name? 
 s.short_code as study_code,
 s.short_name as study_short_name,
 'KF' as program, -- see enum list; should this always be a constant? or should we bother mapping what we have in study.program? 
@@ -27,10 +27,14 @@ count(case when p.visible='true' then participant_id end) as actual_number_of_pa
 'Not Reported' as acknowledgements,
 'Not Reported' as citation_statement, 
 'Not Reported' as doi,
+i.name as contact, 
+s.parent_study_id as parent_study,
+i.name as principle_investigator,
 s.external_id -- using phs number for now
-
-from {{ ref('stable_study')}} s-- think about how this would be adjusted to account for all stable_study tables in KF
+from {{ ref('stable_investigator')}} i 
+join {{ ref('stable_study')}} s on s.investigator_id=i.investigator_id-- think about how this would be adjusted to account for all stable_study tables in KF
 join {{ ref('stable_participant')}} p on p.study_id=s.study_id 
 group by s.study_id, funding_source, study_title, study_code, study_short_name, 
 program, study_description, research_domain, participant_lifespan_age, selection_criteria,
-study_design, clinical_data_source_type, data_category, website, acknowledgements, citation_statement, doi, s.external_id
+study_design, clinical_data_source_type, data_category, website, acknowledgements, citation_statement, doi, s.external_id,
+contact, parent_study, principle_investigator
