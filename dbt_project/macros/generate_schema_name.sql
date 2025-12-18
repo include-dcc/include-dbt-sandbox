b3b98b6b28f46fac48a4ecaa294b301437255b46
Christina Diaz <diazchristina.j@gmail.com>
2025-12-18 17:12:59 -0500

    ♻️ Excl stage from schema name

--- dbt_project/macros/generate_schema_name.sql
+++ dbt_project/macros/generate_schema_name.sql
@@ -1,12 +1,10 @@
 -- macros/generate_schema_name.sql
 {% macro generate_schema_name(custom_schema_name, node) -%}
+# Generate custom schema names based of file structure in dbt_projects # 
 
     {%- set default_schema = target.schema -%}
 
-    {# Get the full file path of the model, excluding the project name and the model name itself #}
-    {%- set fqn_parts = node.fqn[1:-1] -%}
-
-    {# Logic for Seed Files: result will be "dev_schema_import" #}
+    {# 1. Logic for Seed Files: result will be "dev_schema_import" #}
     {%- if node.resource_type == 'seed' -%}
         {%- if custom_schema_name is none -%}
             {{ default_schema | trim }}
@@ -14,12 +12,19 @@
             {{ default_schema | trim }}_{{ custom_schema_name | trim }}
         {%- endif -%}
 
-    {%- elif fqn_parts | length > 0 -%}
-        {# Join the folder names with underscores to create the custom schema part #}
-        {%- set nested_schema = fqn_parts | join('_') -%}
+    {# 2. Logic for Models in subfolders #}
+    {%- elif node.fqn[1:-1] | length > 0 -%}
+        {# Get the subfolders than remove the last one #}
+        {%- set fqn_parts = node.fqn[1:-1] -%}
+        {%- set schema_path_list = fqn_parts[:-1] %}
 
-        {# Combine the default schema and the nested schema part #}
-        {{ default_schema }}_{{ nested_schema }}
+        {# Join the folder names with underscores to create the custom schema part #}
+        {%- set nested_schema = schema_path_list | join('_') -%}
+
+        {# Join only if nested_schema is not empty #}
+        {{ default_schema }}{% if nested_schema | trim != '' %}_{{ nested_schema | trim }}{% endif %}
+
+    {# 3. Logic for models in root or single-depth folders #}
     {%- else -%}
         {# If the model is not in a subfolder, use the default schema #}
         {{ default_schema }}
