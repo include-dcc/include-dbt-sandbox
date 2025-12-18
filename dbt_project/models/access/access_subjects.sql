e6ac57bd50f737d5a1b7bed48a8f3e8aa38ab7de
Amanda Warkow <warkowa@chop.edu>
2025-12-18 15:05:01 -0500

    ✨ add missing field

--- dbt_project/models/access/access_subjects.sql
+++ dbt_project/models/access/access_subjects.sql
@@ -4,6 +4,7 @@
 
 select distinct
     participant_id as subject_id,
-    'KF participant' as subject_type
+    'KF participant' as subject_type, -- adding in KF as a source identifier
+    external_id
     -- organism_type -- not sure if this is needed. could be set to 'human' if needed
 from {{ref('stable_participant')}}
