14209ab14f00fbfc4f1466567b59cc3aa3991b5a
Christina Diaz <diazchristina.j@gmail.com>
2025-12-19 13:57:49 -0500

    🐛 Fix naming
    fix naming for non-nested folders in models

--- dbt_project/macros/generate_schema_name.sql
+++ dbt_project/macros/generate_schema_name.sql
@@ -1,6 +1,5 @@
 -- macros/generate_schema_name.sql
 {% macro generate_schema_name(custom_schema_name, node) -%}
-# Generate custom schema names based of file structure in dbt_projects # 
 
     {%- set default_schema = target.schema -%}
 
@@ -12,22 +11,25 @@
             {{ default_schema | trim }}_{{ custom_schema_name | trim }}
         {%- endif -%}
 
-    {# 2. Logic for Models in subfolders #}
-    {%- elif node.fqn[1:-1] | length > 0 -%}
-        {# Get the subfolders than remove the last one #}
-        {%- set fqn_parts = node.fqn[1:-1] -%}
-        {%- set schema_path_list = fqn_parts[:-1] %}
-
-        {# Join the folder names with underscores to create the custom schema part #}
-        {%- set nested_schema = schema_path_list | join('_') -%}
-
-        {# Join only if nested_schema is not empty #}
-        {{ default_schema }}{% if nested_schema | trim != '' %}_{{ nested_schema | trim }}{% endif %}
-
-    {# 3. Logic for models in root or single-depth folders #}
+    {# 2. Unified Logic for Models #}
     {%- else -%}
-        {# If the model is not in a subfolder, use the default schema #}
-        {{ default_schema }}
+        {# fqn_parts = [folder, subfolder, ..., model_name] #}
+        {%- set fqn_parts = node.fqn[1:-1] -%}
+
+        {%- if fqn_parts | length == 1 -%}
+            {# Case: models/access/file.sql -> output: dev_schema_access #}
+            {{ default_schema }}_{{ fqn_parts[0] }}
+
+        {%- elif fqn_parts | length > 1 -%}
+            {# Case: models/kf/study/src/files.sql -> output: dev_schema_kf_study #}
+            {# Exclude the last subfolder ('src') #}
+            {%- set schema_path = fqn_parts[:-1] | join('_') -%}
+            {{ default_schema }}_{{ schema_path }}
+
+        {%- else -%}
+            {# Fallback for models in the root /models/ folder #}
+            {{ default_schema }}
+        {%- endif -%}
     {%- endif -%}
 
 {%- endmacro %}
