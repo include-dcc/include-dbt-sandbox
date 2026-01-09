413e8e421dbb415d472a7943c8f5c32019affcbe
Christina Diaz <diazchristina.j@gmail.com>
2026-01-09 13:08:51 -0500

    🚚 Moving to sd_test

--- dbt_project/models/kids_first/sd_1nns3k8v/src/src_family.sql
+++
@@ -1,6 +0,0 @@
-{{ config(
-    schema='src'
-) }}
-
-select *
-from {{ ref('sd_1nns3k8v_families') }}
--- dbt_project/models/kids_first/sd_1nns3k8v/src/src_genomic_files.sql
+++
@@ -1,6 +0,0 @@
-{{ config(
-    schema='src'
-) }}
-
-select *
-from {{ ref('sd_1nns3k8v_genomic_files') }}
