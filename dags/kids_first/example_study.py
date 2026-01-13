49f932674a4d621533974f3756376c8172b09717
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2026-01-13 13:08:27 -0500

    🐛 Change environ calls to get

--- dags/kids_first/example_study.py
+++ dags/kids_first/example_study.py
@@ -10,19 +10,19 @@ from cosmos import (
 )
 
 profile_config = ProfileConfig(
-    profile_name=os.environ["DBT_PROFILE_NAME"],
-    profiles_yml_filepath=os.environ["DBT_PROFILES_YML_PATH"],
+    profile_name=os.environ.get("DBT_PROFILE_NAME", "default"),
+    profiles_yml_filepath=os.environ.get("DBT_PROFILES_YML_PATH", "default"),
     target_name="prd",
 )
 
 example_study_dag = DbtDag(
     project_config=ProjectConfig(
-        os.environ["DBT_PROJECT_DIR"],
+        os.environ.get("DBT_PROJECT_DIR", "default"),
         install_dbt_deps=True,
     ),
     profile_config=profile_config,
     execution_config=ExecutionConfig(
-        dbt_executable_path=os.environ["DBT_EXECUTABLE_PATH"],
+        dbt_executable_path=os.environ.get("DBT_EXECUTABLE_PATH", "default"),
     ),
     render_config=RenderConfig(select=["config.meta.study:kf_example_study"]),
     # normal dag parameters
