69a513ea65fa56762010d61a5b79f6b040f940f9
Christina Diaz <diazchristina.j@gmail.com>
2026-01-14 16:33:44 -0500

    ✨ Int tables

---
+++ dbt_project/models/kids_first/dataservice_studies/int/ds_int_phenotypes.sql
@@ -0,0 +1,19 @@
+{{ config(
+    schema='int'
+) }}
+
+select distinct 
+  uuid, -- we can leave this out; it's not used,
+  kf_id as phenotype_id, 
+  lower(replace(kf_id, '_', 'cn-')) as dewrangle_phenotype_id,
+  participant_id,
+  source_text_phenotype, 
+  age_at_event_days,
+  hpo_id_phenotype,
+  observed,
+
+  -- additional fields that may be included
+  snomed_id_phenotype,
+  external_id
+    
+from {{ ref('src_phenotype') }}
