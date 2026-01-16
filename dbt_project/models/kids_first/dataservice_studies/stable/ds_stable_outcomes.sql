a1deb6474243f9c169e2ec4273bd20016e77a758
Christina Diaz <diazchristina.j@gmail.com>
2026-01-16 11:35:10 -0500

    🚧 Stable tables

---
+++ dbt_project/models/kids_first/dataservice_studies/stable/ds_stable_outcomes.sql
@@ -0,0 +1,18 @@
+{{ config(
+    schema='stable'
+) }}
+
+select distinct 
+  uuid, -- we can leave this out, it's not used
+  created_at,
+  modified_at,
+  external_id, -- i think we can leave this out - it's rarely populated/used, 
+  vital_status,
+  disease_related,
+  age_at_event_days,
+  participant_id,
+  kf_id,
+  visible,
+  visibility_reason,
+  visibility_comment,
+from {{ ref('int_outcomes') }}
