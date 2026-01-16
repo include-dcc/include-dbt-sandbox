6994487e59faa8d9864583e05d0c1fe426c56007
Christina Diaz <diazchristina.j@gmail.com>
2026-01-16 11:35:10 -0500

    🚧 Stable tables

---
+++ dbt_project/models/kids_first/dataservice_studies/stable/ds_stable_family_relationships.sql
@@ -0,0 +1,19 @@
+{{ config(
+    schema='stable'
+) }}
+
+select distinct 
+  uuid, -- we can leave this out, it's not used
+  created_at,
+  modified_at,
+  external_id, -- i think we can leave this out - it's rarely populated/used, 
+  participant1_id,
+  participant2_id,
+  participant1_to_participant2_relation,
+  participant2_to_participant1_relation,
+  relationship_id,
+  visible,
+  visibility_reason,
+  visibility_comment,
+  source_text_notes -- don't think we need this? we never populate it
+from {{ ref('src_family_relationships') }}
