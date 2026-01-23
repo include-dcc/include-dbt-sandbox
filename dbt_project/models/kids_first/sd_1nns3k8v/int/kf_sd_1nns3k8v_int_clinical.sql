22e1de3ebd3030dcce973eabd553e6c0312df828
Amanda Warkow <warkowa@chop.edu>
2026-01-23 12:57:36 -0500

    ✨ add int models

--- dbt_project/models/kids_first/sd_1nns3k8v/int/kf_sd_1nns3k8v_int_clinical.sql
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/kf_sd_1nns3k8v_int_clinical.sql
@@ -1,6 +1,21 @@
 {{ config(
-    schema='src'
+    schema='int'
 ) }}
 
-select * 
-from {{ ref('kids_first_update') }}
+select distinct
+    "Family ID" as family_id,
+    "Participant ID" as participant_id,
+    "Family Relationship Target Participant ID" as family_relationship_target_participant_id,
+    "alliquot_id" as aliquot_id,
+    "Consent Group" as consent_group,
+    "Age at Sample" as age_at_sample,
+    "Sample Composition" as sample_composition,
+    "Sample Tissue Type" as sample_tissue_type,
+    "Race" as race,
+    "Sex" as sex,
+    "Ethnicity" as ethnicity,
+    "Analyte Type" as analyte_type,
+    "Age at Phenotype" as age_at_phenotype,
+    "Affected Status" as affected_status, 
+    "Vital Status" as vital_status
+from {{ ref('kf_sd_1nns3k8v_src_clinical') }}
--- dbt_project/models/kids_first/sd_1nns3k8v/int/kf_sd_1nns3k8v_int_s3.sql
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/kf_sd_1nns3k8v_int_s3.sql
@@ -1,6 +1,12 @@
 {{ config(
-    schema='src'
+    schema='int'
 ) }}
 
-select * 
+select distinct
+    "Bucket" as bucket,
+    "Key" as key,
+    "LastModified" as last_modified,
+    "ETag" as etag,
+    "Size" as size,
+    "StorageClass" as storage_class
 from {{ ref('s3_scrape_cody') }}
