3d1eaf1d7235deae88cf086edacb0485b7994c67
Amanda Warkow <warkowa@chop.edu>
2026-01-27 15:18:02 -0500

    ✏️ lint code

--- dbt_project/models/kids_first/sd_1nns3k8v/int/kf_sd_1nns3k8v_int_s3.sql
+++ dbt_project/models/kids_first/sd_1nns3k8v/int/kf_sd_1nns3k8v_int_s3.sql
@@ -3,10 +3,10 @@
 ) }}
 
 select distinct
-    "Bucket" as bucket,
-    "Key" as key,
-    "LastModified" as last_modified,
-    "ETag" as etag,
-    "Size" as size,
-    "StorageClass" as storage_class
+  "Bucket" as bucket,
+  "Key" as key,
+  "LastModified" as last_modified,
+  "ETag" as etag,
+  "Size" as size,
+  "StorageClass" as storage_class
 from {{ ref('s3_scrape_cody') }}
