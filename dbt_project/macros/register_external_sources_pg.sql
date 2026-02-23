7c6d0bf308c82db29ea1dff840e49a598df569a8
brendagutman <brenda.gutman@gmail.com>
2026-02-23 09:09:20 -0600

    Clean up macro

--- dbt_project/macros/register_external_sources_pg.sql
+++ dbt_project/macros/register_external_sources_pg.sql
@@ -1,11 +1,9 @@
 {% macro register_external_sources_pg(tb_schema, tablename, columns, src_data_csv_path) %}
-    -- Validate inputs (optional but recommended)
     {% if tb_schema is none or tablename is none or columns is none or src_data_csv_path is none %}
         {{ log("Error: Missing required arguments in register_external_sources_pg macro.", warn=True) }}
         {% do return('') %}
     {% endif %}
 
-    -- Build the CREATE TABLE statement
     {% set create_table_query %}
     CREATE SCHEMA IF NOT EXISTS {{ tb_schema }};
 
@@ -17,8 +15,6 @@
     {% endset %}
 
 
-
-    -- Combine both queries
     {% set full_query %}
     {{ create_table_query }} 
     {% endset %}
