1e2e1feae95bc46a844faed0ef9ed3a2e9a5c8dc
brendagutman <brenda.gutman@gmail.com>
2026-02-23 09:29:34 -0600

    Add studies to run commands

--- dbt_project/run_commands/include/aadsc/run_20260119_aadsc.sh
+++ dbt_project/run_commands/include/aadsc/run_20260119_aadsc.sh
@@ -4,6 +4,8 @@ dbt deps || { echo "Error: dbt deps failed. Exiting..."; exit 1; }
 dbt seed #--full-refresh
 # Source tables
 dbt run --select include_aadsc_src_chicoine_down_syndrome_extract
+dbt run --select studies
+
 
 dbt run --select aadsc_test_participant
 dbt run --select aadsc_test_condition
