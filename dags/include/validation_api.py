5237bcb705caf4967c1156595ad424b7282e53de
j-kanuch <joshua.d.kanuch@vumc.org>
2026-05-05 11:58:04 -0500

    initial commit

---
+++ dags/include/validation_api.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+from airflow.sdk import Variable
+import os
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.models.dag import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+def run_validation(**context):
+    print("Run dataset validation here")
+
+
+with DAG(
+    "dataset_validation",
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'on_skipped_callback': another_function, #or list of functions
+        # 'trigger_rule': 'all_success'
+    },
+    description="Run validation against payload",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["POC", "validation"],
+) as dag:
+
+    t1 = PythonOperator(
+        task_id="validate_dataset",
+        python_callable=run_validation,
+    )
