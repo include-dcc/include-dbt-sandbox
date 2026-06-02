7604ed2992b0416b2aab238a1ac16a325b50fa93
brendagutman <brenda.gutman@gmail.com>
2026-06-02 11:23:15 -0500

    Linting access and export models

--- dbt_project/models/export/fhir/fhir_researchstudycollection_label.sql
+++ dbt_project/models/export/fhir/fhir_researchstudycollection_label.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
-    select
-    researchstudycollection_research_study_collection_id::text as "ResearchStudyCollection_research_study_collection_id",
-    label::text as "label"
-    {# from {{ ref('') }} #} -- COMMENTED OUT TO AVOID ERROR, REPLACE WITH APPROPRIATE REF
+select
+  researchstudycollection_research_study_collection_id::text as "ResearchStudyCollection_research_study_collection_id",
+  label::text as label
+{# from {{ ref('') }} #} -- COMMENTED OUT TO AVOID ERROR, REPLACE WITH APPROPRIATE REF
     
