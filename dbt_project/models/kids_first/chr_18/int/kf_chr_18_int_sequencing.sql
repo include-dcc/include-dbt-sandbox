756383fe5f9882ada8f965922709f4dd0c661f84
Amanda Warkow <warkowa@chop.edu>
2026-06-29 15:05:20 -0400

    ✨ int model for terra manifest

--- dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_sequencing.sql
+++ dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_sequencing.sql
@@ -1,19 +1,55 @@
 {{ config(materialized='table') }}
 
-    select
-    kf_index,
-    type,
-    sample,
-    file_name,
-    file_type,
-    file_description,
-    file_size,
-    md5sum,
-    library_id,
-    library_strategy,
-    library_layout,
-    library_source,
-    platform,
-    instrument_model
-    from {{ ref('kf_chr_18_src_ha_delivery_manifest_6735') }}
-    
+    with base as (
+        select
+            sample_id,
+            collaborator_sample_id,
+            data_type,
+            collaborator_participant_id,
+            root_sample_id,
+            total_reads,
+            mean_read_length,
+            reference_sequence_name
+        from {{ ref('kf_chr_18_src_terra_manifest_v2') }}
+    ),
+
+    crams as (
+        select
+        root_sample_id,
+        cram_path
+        from {{ ref('kf_chr_18_src_terra_manifest_v2') }} 
+    ),
+
+    crais as (
+        select
+        root_sample_id,
+        crai_path
+        from {{ ref('kf_chr_18_src_terra_manifest_v2') }} as crais
+    ),
+
+    md5s as (
+        select
+        root_sample_id,
+        md5_path
+        from {{ ref('kf_chr_18_src_terra_manifest_v2') }} as md5s
+    )
+
+    select 
+    base.sample_id,
+    base.collaborator_sample_id,
+    base.data_type,
+    base.collaborator_participant_id,
+    base.root_sample_id,
+    base.total_reads,
+    base.mean_read_length,
+    base.reference_sequence_name,
+    crams.cram_path,
+    crais.crai_path,
+    md5s.md5_path
+    from base
+    left join crams
+    on base.root_sample_id = crams.root_sample_id
+    left join crais
+    on base.root_sample_id = crais.root_sample_id
+    left join md5s
+    on base.root_sample_id = md5s.root_sample_id
