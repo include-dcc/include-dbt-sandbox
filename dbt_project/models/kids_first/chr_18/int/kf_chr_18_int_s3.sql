4463f6ade22149e5eb227895a8984a3d1ae37d0b
Amanda Warkow <warkowa@chop.edu>
2026-06-30 13:50:30 -0400

    ✨ add in ingest transformations

--- dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_s3.sql
+++ dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_s3.sql
@@ -1,12 +1,19 @@
 {{ config(materialized='table') }}
 
     select
-    kf_index,
-    size,
-    key,
-    etag,
-    bucket,
-    lastmodified,
-    storageclass
+    's3://' + bucket + '/' + key as s3_path,
+    case
+        when key.like '%.cram' then 'cram'
+        when key.like '%.crai' then 'crai'
+        when key.like '%.md5' then 'md5'
+        else null
+    end as file_format,
+    case
+        when key.like '%.cram' then 'Aligned Reads'
+        when key.like '%.crai' then 'Aligned Reads Index'
+        when key.like '%.md5' then 'Other'
+        else null
+    end as data_type,
+    '{etag' + replace(etag, '"', "") + '}' as hash_dict
     from {{ ref('kf_chr_18_src_s3_scrape_cody') }}
     
--- dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_sequencing.sql
+++ dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_sequencing.sql
@@ -2,11 +2,11 @@
 
     with base as (
         select
-            sample_id,
-            collaborator_sample_id,
-            data_type,
-            collaborator_participant_id,
-            root_sample_id,
+            -- sample_id,
+            -- collaborator_sample_id,
+            collaborator_participant_id as participant_id,
+            root_sample_id as sample_id,
+            data_type as sequencing_strategy,
             total_reads,
             mean_read_length,
             reference_sequence_name
@@ -16,40 +16,52 @@
     crams as (
         select
         root_sample_id,
-        cram_path
+        replace(cram_path,
+                "gs://fc-2092c58b-ce12-42e7-9a8e-64b78440537a/",
+                "s3://kf-strides-study-us-east-1-prd-sd-1nns3k8v/source/",
+                ) as cram_path
         from {{ ref('kf_chr_18_src_terra_manifest_v2') }} 
     ),
 
     crais as (
         select
         root_sample_id,
-        crai_path
+        replace(crai_path,
+                "gs://fc-2092c58b-ce12-42e7-9a8e-64b78440537a/",
+                "s3://kf-strides-study-us-east-1-prd-sd-1nns3k8v/source/",
+                ) as crai_path
         from {{ ref('kf_chr_18_src_terra_manifest_v2') }} as crais
     ),
 
     md5s as (
         select
         root_sample_id,
-        md5_path
+        replace(md5_path,
+                "gs://fc-2092c58b-ce12-42e7-9a8e-64b78440537a/",
+                "s3://kf-strides-study-us-east-1-prd-sd-1nns3k8v/source/",
+                ) as md5_path
         from {{ ref('kf_chr_18_src_terra_manifest_v2') }} as md5s
     )
 
     select 
+    -- base.sample_id,
+    -- base.collaborator_sample_id,
+    base.participant_id,
     base.sample_id,
-    base.collaborator_sample_id,
-    base.data_type,
-    base.collaborator_participant_id,
-    base.root_sample_id,
+    base.sequencing_strategy,
     base.total_reads,
     base.mean_read_length,
     base.reference_sequence_name,
     crams.cram_path,
     crais.crai_path,
-    md5s.md5_path
+    md5s.md5_path,
+    'False' as harmonized,
+    'False' as paired_end,
+    'Broad Institute' as sequencing_center_name
     from base
     left join crams
-    on base.root_sample_id = crams.root_sample_id
+    on base.sample_id = crams.root_sample_id
     left join crais
-    on base.root_sample_id = crais.root_sample_id
+    on base.sample_id = crais.root_sample_id
     left join md5s
-    on base.root_sample_id = md5s.root_sample_id
+    on base.sample_id = md5s.root_sample_id
