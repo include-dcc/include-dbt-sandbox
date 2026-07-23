4c028ff990e36693cae48a2069051e6ce0329ab9
brendagutman <brenda.gutman@gmail.com>
2026-07-23 13:58:36 -0500

    Edit the column name in the familyrelationship access model

--- dbt_project/models/access/access_familyrelationship.sql
+++ dbt_project/models/access/access_familyrelationship.sql
@@ -3,7 +3,7 @@
 select
   family_relationship_id::text as family_relationship_id,
   family_member_id::text as family_member_id,
-  relationship::text as relationship,
+  relationship::text as relationship_type,
   subject_id::text as subject_id,
   access_policy_id::text as access_policy_id,
   study_id::text as study_id
