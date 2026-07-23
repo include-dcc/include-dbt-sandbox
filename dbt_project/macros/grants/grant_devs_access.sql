67b3955bb4c0d39cb0a908df3df0d0eaf29d7cdc
brendagutman <brenda.gutman@gmail.com>
2026-07-23 14:17:16 -0500

    Modify to use on all to grant permission

--- dbt_project/macros/grants/grant_devs_access.sql
+++ dbt_project/macros/grants/grant_devs_access.sql
@@ -1,5 +1,13 @@
 {% macro grant_devs_access(tag, users_role) %}
   {% if execute %}
+    {#
+      Use explicit table privileges instead of GRANT ALL to follow least-privilege.
+      In Postgres, ALL TABLE privileges also include TRIGGER, REFERENCES, and TRUNCATE,
+      which are broader than required for this workflow.
+    #}
+    {% set table_privileges = 'select, insert, update, delete' %}
+    {% set sequence_privileges = 'usage, select' %}
+
     {% set command_name = flags.WHICH if flags is defined and flags.WHICH is defined else none %}
     {% if command_name not in ['run', 'build'] %}
       {% do log('grant_devs_access: skipping for command ' ~ (command_name or 'unknown') ~ '; grants are only applied for run/build', info=True) %}
@@ -49,11 +57,11 @@
         {% do log('Applying ' ~ grantee_name ~ ' grants on schema ' ~ schema_name, info=True) %}
 
         {% do run_query("grant usage on schema " ~ quoted_schema ~ " to " ~ grantee) %}
-        {% do run_query("grant select, insert, update, delete on all tables in schema " ~ quoted_schema ~ " to " ~ grantee) %}
-        {% do run_query("grant usage, select on all sequences in schema " ~ quoted_schema ~ " to " ~ grantee) %}
+        {% do run_query("grant " ~ table_privileges ~ " on all tables in schema " ~ quoted_schema ~ " to " ~ grantee) %}
+        {% do run_query("grant " ~ sequence_privileges ~ " on all sequences in schema " ~ quoted_schema ~ " to " ~ grantee) %}
 
-        {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant select, insert, update, delete on tables to " ~ grantee) %}
-        {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant usage, select on sequences to " ~ grantee) %}
+        {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant " ~ table_privileges ~ " on tables to " ~ grantee) %}
+        {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant " ~ sequence_privileges ~ " on sequences to " ~ grantee) %}
       {% else %}
         {% do log('Skipping grants for schema ' ~ schema_name ~ ' (already granted)', info=True) %}
       {% endif %}
--- dbt_project/macros/grants/grant_schema_role_access.sql
+++ dbt_project/macros/grants/grant_schema_role_access.sql
@@ -1,5 +1,13 @@
 {% macro grant_schema_role_access(target_schema, users_role) %}
   {% if execute %}
+    {#
+      Use explicit table privileges instead of GRANT ALL to follow least-privilege.
+      In Postgres, ALL TABLE privileges also include TRIGGER, REFERENCES, and TRUNCATE,
+      which are broader than required for this workflow.
+    #}
+    {% set table_privileges = 'select, insert, update, delete' %}
+    {% set sequence_privileges = 'usage, select' %}
+
     {% if not target_schema %}
       {% do exceptions.raise_compiler_error('grant_schema_role_access: target_schema is required') %}
     {% endif %}
@@ -27,11 +35,11 @@
       {% do log('Applying ' ~ grantee_name ~ ' grants on schema ' ~ schema_name, info=True) %}
 
       {% do run_query("grant usage on schema " ~ quoted_schema ~ " to " ~ grantee) %}
-      {% do run_query("grant select, insert, update, delete on all tables in schema " ~ quoted_schema ~ " to " ~ grantee) %}
-      {% do run_query("grant usage, select on all sequences in schema " ~ quoted_schema ~ " to " ~ grantee) %}
+      {% do run_query("grant " ~ table_privileges ~ " on all tables in schema " ~ quoted_schema ~ " to " ~ grantee) %}
+      {% do run_query("grant " ~ sequence_privileges ~ " on all sequences in schema " ~ quoted_schema ~ " to " ~ grantee) %}
 
-      {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant select, insert, update, delete on tables to " ~ grantee) %}
-      {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant usage, select on sequences to " ~ grantee) %}
+      {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant " ~ table_privileges ~ " on tables to " ~ grantee) %}
+      {% do run_query("alter default privileges in schema " ~ quoted_schema ~ " grant " ~ sequence_privileges ~ " on sequences to " ~ grantee) %}
     {% else %}
       {% do log('Skipping grants for schema ' ~ schema_name ~ ' (already granted for role ' ~ grantee_name ~ ')', info=True) %}
     {% endif %}
