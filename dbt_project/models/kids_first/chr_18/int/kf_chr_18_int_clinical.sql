93a6b863e1c12cba01588cce906dfeca13cee67b
Amanda Warkow <warkowa@chop.edu>
2026-07-27 14:15:38 -0400

    ✨ map to constants

--- dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_clinical.sql
+++ dbt_project/models/kids_first/chr_18/int/kf_chr_18_int_clinical.sql
@@ -4,14 +4,27 @@
         participant_id,
         family_relationship_target_participant_id,
         aliquot_id,
-        consent_group,
+        'GRU' as consent_group,
         age_at_sample,
-        sample_composition,
-        race,
-        sex,
-        ethnicity,
+        case
+            when sample_composition is 'Blood' then 'Peripheral Whole Blood'
+            when sample_composition is 'Lymphoblastoid Cells' then 'Lymphoblastoid Cell Lines'
+        end as sample_composition,
+        case
+            when race is 'Native American' then 'American Indian or Alaska Native'
+            when race is 'Caucasian' then 'White'
+            else race
+        end as race,
+        case
+            when sex is 'M' then 'Male'
+            when sex is 'F' then 'Female'
+        end as sex,
+        case
+            when ethnicity is 'Not Hispanic' then 'Not Hispanic or Latino'
+            when ethnicity is 'Hispanic' then 'Hispanic or Latino'
+        end as ethnicity,
         analyte_type,
-        age_at_phenotype,
+        -- age_at_phenotype,
         affected_status,
         vital_status,
         sample_tissue_type
