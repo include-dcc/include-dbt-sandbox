b96edca610d5a312a4c744ceda02fb132fbb5930
Amanda Warkow <warkowa@chop.edu>
2026-07-27 17:07:17 -0400

    ✏️ sqlfluff linting

--- dbt_project/models/kids_first/chr_18/src/S3_Bucket_Scrape/kf_chr_18_src_s3_scrape_cody.sql
+++ dbt_project/models/kids_first/chr_18/src/S3_Bucket_Scrape/kf_chr_18_src_s3_scrape_cody.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='table') }}
 
-    select
-    ROW_NUMBER() OVER () AS kf_index,
-    "Size"::bigint as size,
-    "Key"::text as key,
-    "ETag"::text as etag,
-    "Bucket"::text as bucket,
-    "LastModified"::text as lastmodified,
-    "StorageClass"::text as storageclass
-    from {{ source('chr_18', 's3_scrape_cody') }}
+select
+  ROW_NUMBER() over () as kf_index,
+  "Size"::bigint as size,
+  "Key"::text as key,
+  "ETag"::text as etag,
+  "Bucket"::text as bucket,
+  "LastModified"::text as lastmodified,
+  "StorageClass"::text as storageclass
+from {{ source('chr_18', 's3_scrape_cody') }}
     
--- dbt_project/models/kids_first/chr_18/stb/kf_chr_18_stb_concept.sql
+++ dbt_project/models/kids_first/chr_18/stb/kf_chr_18_stb_concept.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
-    select
-    null::text as "concept_curie",
-    null::text as "display"
-    from {{ ref('kf_chr_18_src_chr_18_phenotype_data') }}
+select
+  null::text as concept_curie,
+  null::text as display
+from {{ ref('kf_chr_18_src_chr_18_phenotype_data') }}
     
